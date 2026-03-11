167b97b959261c2105bf0809da49f02c2d2b015f
BrickMemer <krzy2354@gmail.com>
2026-03-11 11:55:57 +0100

    added paddin gclass

---
+++ padding.cpp
@@ -0,0 +1,3 @@
+#include "padding.hpp"
+
+padding::padding() {}
---
+++ padding.hpp
@@ -0,0 +1,11 @@
+#ifndef PADDING_HPP
+#define PADDING_HPP
+#include <string>
+
+class padding
+{
+public:
+    static std::string PaddingLeft(int amount);
+};
+
+#endif // PADDING_HPP
